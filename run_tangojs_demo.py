be67b5d131c1cd7e25b1f12cb696f90446f8c48d
Marcin Migda <marcin700@gmail.com>
2017-03-30 21:42:45 +0200

    Download, install and run server with template tangojs

--- run_tangojs_demo.py
+++ run_tangojs_demo.py
@@ -6,13 +6,16 @@ def git(*args):
 
 
 def npm(*args, folder=None):
-    print(folder)
     npm_process = subprocess.Popen(['npm'] + list(args), cwd=folder)
     return npm_process.communicate()
 
 
 if __name__ == "__main__":
     git("clone", "git://github.com/tangojs/tangojs-webapp-template")
+    npm("install", folder="tangojs-webapp-template")
+
     npm("install", "--save", "tangojs-core", folder="tangojs-webapp-template")
     npm("install", "--save", "tangojs-connector-local", folder="tangojs-webapp-template")
     npm("install", "--save", "tangojs-web-components", folder="tangojs-webapp-template")
+
+    npm("run", "server", folder="tangojs-webapp-template")
