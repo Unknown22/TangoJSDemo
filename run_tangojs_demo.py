8d4646ef094ab5dd00917afcbf05a953774f9993
Marcin Migda <marcin700@gmail.com>
2017-03-30 21:52:57 +0200

    Run browser with running demo when everything ready

--- run_tangojs_demo.py
+++ run_tangojs_demo.py
@@ -1,4 +1,5 @@
 import subprocess
+from time import sleep
 
 
 def git(*args):
@@ -7,15 +8,17 @@ def git(*args):
 
 def npm(*args, folder=None):
     npm_process = subprocess.Popen(['npm'] + list(args), cwd=folder)
-    return npm_process.communicate()
+    return npm_process
 
 
 if __name__ == "__main__":
     git("clone", "git://github.com/tangojs/tangojs-webapp-template")
-    npm("install", folder="tangojs-webapp-template")
+    npm("install", folder="tangojs-webapp-template").communicate()
 
-    npm("install", "--save", "tangojs-core", folder="tangojs-webapp-template")
-    npm("install", "--save", "tangojs-connector-local", folder="tangojs-webapp-template")
-    npm("install", "--save", "tangojs-web-components", folder="tangojs-webapp-template")
+    npm("install", "--save", "tangojs-core", folder="tangojs-webapp-template").communicate()
+    npm("install", "--save", "tangojs-connector-local", folder="tangojs-webapp-template").communicate()
+    npm("install", "--save", "tangojs-web-components", folder="tangojs-webapp-template").communicate()
 
-    npm("run", "server", folder="tangojs-webapp-template")
+    server_process = npm("run", "server", folder="tangojs-webapp-template")
+    sleep(3)
+    browser = subprocess.Popen(['firefox'] + ['127.0.0.1:8081'])
