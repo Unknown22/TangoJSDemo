1c96ef9e9b172c0bb3d42792e0ff647e7a066d43
Marcin Migda <marcin700@gmail.com>
2017-03-31 01:33:47 +0200

    Check and install n and node. No auto update for firefox yet

--- run_tangojs_demo.py
+++ run_tangojs_demo.py
@@ -3,7 +3,7 @@ from time import sleep
 
 
 def git(*args):
-    return subprocess.check_call(['git'] + list(args))
+    out, err = subprocess.Popen(['git'] + list(args)).communicate()
 
 
 def npm(*args, folder=None):
@@ -25,38 +25,96 @@ def _install_and_run():
 def _check_requirements():
     requirements = []
     requirements.append(_check_npm())
+    requirements.append(_check_node())
     if False in requirements:
         return False
     return True
 
+def _check_node():
+    try:
+        node_version = _check_node_version()
+        if int(node_version) < 7:
+            node_update_answer = input("You need update node. Do you want to do it now? (y/N): ")
+            if node_update_answer.lower() == 'y':
+                n_install_process = subprocess.Popen(['sudo', 'npm', 'install', '-g', 'n'])
+                out, err = n_install_process.communicate()
+                node_update_process = subprocess.Popen(['sudo', 'n', 'stable'])
+                out, err = node_update_process.communicate()
+                node_version = _check_node_version()
+                if int(node_version) < 7:
+                    print("Something went wrong with node update. Try to do it manually and run this script again.")
+                    return False
+                return True
+            else:
+                return False
+    except:
+        print("Something went wrong with node check/update")
+        return False
+
+
+def _check_node_version():
+    npm_version_check = subprocess.Popen(['node', '-v'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = npm_version_check.communicate()
+    npm_version = out.decode('utf-8')
+    if npm_version == '':
+        return 0
+    return npm_version[1]
+
 
 def _check_npm():
-    npm_version = _check_npm_version()
-    if int(npm_version[0]) < 4:
+    npm_version = None
+
+    try:
+        npm_version = _check_npm_version()
+    except FileNotFoundError:
+        npm_install_answer = input("You don't have npm. Do you want to install it now? (y/N): ")
+        if npm_install_answer.lower() == 'y':
+            npm_install = subprocess.Popen(['sudo', 'apt-get', 'install', 'npm'])
+            our, err = npm_install.communicate()
+
+    if npm_version is None or npm_version == 0:
+        try:
+            npm_version = _check_npm_version()
+            if npm_version == 0:
+                symlink_answer = input("Path for node changed after upgrade. Do you want to fix it with symlink? (y/N) :")
+                if symlink_answer.lower() == 'y':
+                    symlink_process = subprocess.Popen(['sudo', 'ln', '-s', '/usr/bin/nodejs', '/usr/bin/node'])
+                    out, err = symlink_process.communicate()
+                    npm_version = _check_npm_version()
+                else:
+                    return False
+        except FileNotFoundError:
+            print("Something went wrong with npm install. Try to do it manually and run this script again.")
+            return False
+
+    if int(npm_version) < 4:
         print("You must upgrade npm and nodejs.")
         update_npm_answer = input("Do you want to do it now? (y/N): ")
         if update_npm_answer.lower() == 'y':
             print("npm update start")
             npm_update = subprocess.Popen(['sudo', 'npm', 'install', '-g', 'npm'])
             out, err = npm_update.communicate()
-            if int(_check_npm_version()[0]) >= 4:
+            if int(_check_npm_version()) >= 4:
                 return True
             else:
                 print("Something went wrong with upgrade.")
-    return False
+                return False
+    else:
+        return True
 
 
 def _check_npm_version():
     npm_version_check = subprocess.Popen(['npm', '-v'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = npm_version_check.communicate()
     npm_version = out.decode('utf-8')
-    return npm_version
+    if npm_version == '':
+        return 0
+    return npm_version[0]
 
 
 if __name__ == "__main__":
     if _check_requirements():
         print("OK")
-        pass
+        _install_and_run()
     else:
         print("You do not meet requirements.")
-        # _install_and_run()
