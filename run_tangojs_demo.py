73e77ab20a172bc9db7cff3fe4701092f2121ba0
Marcin Migda <marcin700@gmail.com>
2017-04-03 21:26:25 +0200

    Fixed bug with update on Ubuntu

--- run_tangojs_demo.py
+++ run_tangojs_demo.py
@@ -53,54 +53,65 @@ def _check_node():
 
 
 def _check_node_version():
-    npm_version_check = subprocess.Popen(['node', '-v'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = npm_version_check.communicate()
-    npm_version = out.decode('utf-8')
-    if npm_version == '':
+    node_version_check = subprocess.Popen(['node', '-v'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = node_version_check.communicate()
+    node_version = out.decode('utf-8')
+    if node_version == '':
         return 0
-    return npm_version[1]
+    return node_version[1]
 
 
 def _check_npm():
-    npm_version = None
-
     try:
-        npm_version = _check_npm_version()
+        if 0 < int(_check_npm_version()) < 4:
+            if not upgrade_npm():
+                return False
+
+        if int(_check_npm_version()) == 0:
+            _fix_npm_symlink()
+
+        if int(_check_npm_version()) >= 4:
+            return True
     except FileNotFoundError:
         npm_install_answer = input("You don't have npm. Do you want to install it now? (y/N): ")
         if npm_install_answer.lower() == 'y':
             npm_install = subprocess.Popen(['sudo', 'apt-get', 'install', 'npm'])
-            our, err = npm_install.communicate()
+            out, err = npm_install.communicate()
 
-    if npm_version is None or npm_version == 0:
-        try:
-            npm_version = _check_npm_version()
-            if npm_version == 0:
-                symlink_answer = input("Path for node changed after upgrade. Do you want to fix it with symlink? (y/N) :")
-                if symlink_answer.lower() == 'y':
-                    symlink_process = subprocess.Popen(['sudo', 'ln', '-s', '/usr/bin/nodejs', '/usr/bin/node'])
-                    out, err = symlink_process.communicate()
-                    npm_version = _check_npm_version()
-                else:
+            if 0 < int(_check_npm_version()) < 4:
+                if not upgrade_npm():
                     return False
-        except FileNotFoundError:
-            print("Something went wrong with npm install. Try to do it manually and run this script again.")
-            return False
 
-    if int(npm_version) < 4:
-        print("You must upgrade npm and nodejs.")
-        update_npm_answer = input("Do you want to do it now? (y/N): ")
-        if update_npm_answer.lower() == 'y':
-            print("npm update start")
-            npm_update = subprocess.Popen(['sudo', 'npm', 'install', '-g', 'npm'])
-            out, err = npm_update.communicate()
+            if int(_check_npm_version()) == 0:
+                _fix_npm_symlink()
+
             if int(_check_npm_version()) >= 4:
                 return True
             else:
-                print("Something went wrong with upgrade.")
                 return False
+
+
+def upgrade_npm():
+    print("You must upgrade npm")
+    update_npm_answer = input("Do you want to do it now? (y/N): ")
+    if update_npm_answer.lower() == 'y':
+        print("npm update start")
+        npm_update = subprocess.Popen(['sudo', 'npm', 'install', '-g', 'npm'])
+        out, err = npm_update.communicate()
+        if int(_check_npm_version()) >= 0:
+            return True
+        else:
+            print("Something went wrong with upgrade.")
+            return False
     else:
-        return True
+        return False
+
+
+def _fix_npm_symlink():
+    symlink_answer = input("Path for node changed after upgrade. Do you want to fix it with symlink? (y/N) :")
+    if symlink_answer.lower() == 'y':
+        symlink_process = subprocess.Popen(['sudo', 'ln', '-s', '/usr/bin/nodejs', '/usr/bin/node'])
+        out, err = symlink_process.communicate()
 
 
 def _check_npm_version():
