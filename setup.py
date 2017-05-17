a409f8f41f254cfd5716463ec04090f4d055c082
Lukasz Zytniak <lzytniak@gmail.com>
2017-05-17 18:02:20 +0200

    Update setup conf

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup, find_packages
 
+# Read function
+def safe_read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except IOError:
+        return ""
+
+# Setup
 setup(name="tangojs-demo",
       version="0.1.1",
       description="Demo setup for TangoJS",
       license="GPLv3",
+      long_description=safe_read("README.md"),
 #      package_dir={'': 'tangojsdemo', },
       include_package_data=True,
       packages=find_packages(),
       package_data={'tangojsdemo': ['images/*.png']},
       data_files=[('/usr/share/applications', ['tangojs-demo.desktop'])],
-      scripts=['script/runtangojsdemo']
+      scripts=['scripts/runtangojsdemo']
       )
